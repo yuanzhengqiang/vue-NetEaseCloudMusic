be7eb23d23d084b95e309d2d8a1b774b48e6d413
yuanzhengqiang <yuanzhengqiang@foxmail.com>
2017-12-20 17:54:34 +0800

    修改run地址

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-start cmd /k "cd E:\Commonality Demo\NeteaseCloudMusicApi&&npm run start"
-start cmd /k "cd E:\Commonality Demo\NeteaseCloudMusic&&npm run start"
+start cmd /k "cd E:\GitHub\vue-NetEaseCloudMusic\NeteaseCloudMusicApi&&npm run start"
+start cmd /k "cd E:\GitHub\vue-NetEaseCloudMusic\NetEaseCloudMusic&&npm run start"
